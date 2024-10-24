3a2c5b467ff22732f5b06071e88af6c54d76608c
Candace Chung <candace_chung@Candaces-MacBook-Pro.local>
2024-10-24 18:58:56 +0100

    optimised interpolatedDataFrame function

--- modules.py
+++ modules.py
@@ -13,6 +13,8 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import RegularPolygon
 import pylab as pl
 import mpl_toolkits.mplot3d.art3d as art3d
+from matplotlib.animation import FuncAnimation
+from joblib import Parallel, delayed
 
 #%%
 
@@ -40,35 +42,8 @@ class loadData:
             time = np.arange(0, signals.shape[0] * t_interval, t_interval)
             return x, y, coord, signals, time
 
-        def interpolatedDataFrame(self, x_df, y_df, coord_pairs, signal_df, time_arr): #all arrays must be stored as lists of lists, so they need to be converted back to arrays later
-            """
-            This function generates the dataframe with the interpolated data
-            
-            The dataframe is composed of a series of concatenated sub-dataframes:
-            - Dataframe with key "X": X coordinates of meshgrid grid points
-            - Dataframe with key "Y": Y coordinates of meshgrid grid points
-            - Dataframes with timestamps as keys: interpolated signal at each grid point
-            
-            The dimensions of the sub-dataframes correspond to the dimensions of the meshgrid
-            """
-            N = n * 1j #include electrode positions and signals in the grid
-            Xarr, Yarr = np.mgrid[x_df.min():x_df.max():N, y_df.min():y_df.max():N] #create spatial meshgrid for interpolation
-            Xdf, Ydf = pd.DataFrame(Xarr), pd.DataFrame(Yarr) #turn the arrays into dataframes to speed code up
-            
-            df_list = [Xdf, Ydf]
-            df_keys = ["X", "Y"]
-            for i in range(len(time_arr)):
-                signal = signal_df.iloc[i, :]
-                Zarr = spi.griddata(coord_pairs, signal, (Xarr, Yarr), method = 'cubic') #cubic spline interpolation for electrical signals
-                Zdf = pd.DataFrame(Zarr) #turn the interpolated signal array into dataframes to speed code up
-                df_list.append(Zdf)
-                df_keys.append(time_arr[i])
-            df = pd.concat(df_list, axis = 0, keys = df_keys) #concatenate all the X, Y, and Z dataframes at different times and label them by the time
-            return df
-        
+        self.n = n
         self.x_pos, self.y_pos, self.coord, self.signals, self.time = electrodeData(self, data)
-        
-        self.df = interpolatedDataFrame(self, self.x_pos, self.y_pos, self.coord, self.signals, self.time)
     
     def x_position(self):
         """
@@ -104,27 +79,54 @@ class loadData:
         return self.time
     
     def data_frame(self):
-        """
-        This function calls the data frame of interpolated data outside of the class
+        def interpolatedDataFrame(self, x_df, y_df, coord_pairs, signal_df, time_arr):
+            """
+            This function generates the dataframe with the interpolated data
+                
+            The dataframe is composed of a series of concatenated sub-dataframes:
+            - Dataframe with key "X": X coordinates of meshgrid grid points
+            - Dataframe with key "Y": Y coordinates of meshgrid grid points
+            - Dataframes with timestamps as keys: interpolated signal at each grid point
+                
+            The dimensions of the sub-dataframes correspond to the dimensions of the meshgrid
+            """
+            def interpolateSignal(signal_array, coordinate_pairs, X_array, Y_array, ind): #function to interpolate the signals
+                signal = signal_array[ind, :]
+                Zarr = spi.griddata(coordinate_pairs, signal, (X_array, Y_array), method='cubic') #cubic spline interpolation for electrical signals
+                Zdf = pd.DataFrame(Zarr) #turn the interpolated signal array into dataframe
+                return Zdf
+            
+            N = self.n * 1j
+            Xarr, Yarr = np.mgrid[x_df.min():x_df.max():N, y_df.min():y_df.max():N] #create spatial meshgrid for interpolation
+            Xdf, Ydf = pd.DataFrame(Xarr), pd.DataFrame(Yarr) #turn the arrays into dataframes
+            
+            signal_arr = signal_df.to_numpy() #convert the signal array into a dataframe for faster processing
+            
+            df_list = [Xdf, Ydf] #create list of dataframes for concatenation
+            df_keys = ["X", "Y"] #create list of keys to call concatenated dataframes
+
+            # Use parallelism to speed up interpolation across time steps
+            results = Parallel(n_jobs=-1)(delayed(interpolateSignal)(signal_arr, coord_pairs, Xarr, Yarr, i) for i in range(len(time_arr))) #get the interpolated signal dataframes over all time
+
+            df_list.extend(results) #add the signal dataframes to the dataframe list for concatenation
+            df_keys.extend(time_arr) #add the times to the list of keys for concatenation
+
+            df = pd.concat(df_list, axis=0, keys=df_keys) #concatenate all the X, Y, and Z dataframes at different times and label them by the time
+            return df
         
-        The dataframe is composed of a series of concatenated sub-dataframes:
-        - Dataframe with key "X": X coordinates of meshgrid grid points
-        - Dataframe with key "Y": Y coordinates of meshgrid grid points
-        - Dataframes with timestamps as keys: interpolated signal at each grid point
-        
-        The dimensions of the sub-dataframes correspond to the dimensions of the meshgrid
-        """
-        return self.df
+        data_frame = interpolatedDataFrame(self, self.x_pos, self.y_pos, self.coord, self.signals, self.time)
+        return data_frame
 
 class Animate(loadData):
-    def __init__(self, data, n, ind, ele_radius, animate = False): #create template for figure and mesh grid
+    def __init__(self, data, n, dataframe, ind, ele_radius, animate = False): #create template for figure and mesh grid
         loadData.__init__(self, data, n)
         
+        self.df = dataframe
         self.ind = ind
         self.ele_radius = ele_radius
         self.animate = animate
         
-        self.X, self.Y = self.df.loc["X"], self.df.loc["Y"]
+        self.X, self.Y = self.df.loc["X"].to_numpy(), self.df.loc["Y"].to_numpy()
         
         self.fig = pl.figure()
         self.ax = self.fig.add_subplot(projection = '3d')
@@ -140,8 +142,8 @@ class Animate(loadData):
     
     def run(self): #runs the animation or returns frame in animation at time with index = ind
         def plot_ith_Frame(self, ind): #plots ith frame
-            signal = self.signals.iloc[ind, :]
-            Z = self.df.loc[self.time[ind]]
+            signal = self.signals.iloc[ind, :].to_numpy()
+            Z = self.df.loc[self.time[ind]].to_numpy()
             plot = self.ax.plot_surface(self.X, self.Y, Z, alpha = 0.2, fc = 'w', ec = 'k', shade = False) #plot surface
             for i, ele in enumerate(self.electrodes): #update vertical position of all electrodes
                 ele._segment3d = [(x, y, signal[i]) for x, y, _ in ele._segment3d]
@@ -160,5 +162,4 @@ class Animate(loadData):
                 plot.remove()
         else:
             plot = plot_ith_Frame(self, self.ind) #plot graph at specific point in time
-            
-# %%
+
--- testing.py
+++ testing.py
@@ -6,19 +6,39 @@ Created on Fri Oct 11 20:02:20 2024
 @author: candace_chung
 """
 
+import time
+
 from modules import *
 
 data = "ElectrogramData.xlsx"
 
 #%%
+start = time.time()
 
 L = loadData(data, n = 600)
 
 df = L.data_frame()
+
+end = time.time()
+
+print("time take (new)", end - start)
+
+#%%
+
 print(df)
 
 #%%
 
-A = Animate(data, n = 600, ind = 218, ele_radius = 0.5, animate = True)
+start = time.time()
+
+L = loadData(data, n = 600)
+
+df = L.data_frame()
+
+A = Animate(data, n = 600, dataframe = df, ind = 218, ele_radius = 0.5, animate = True)
 
 A.run()
+
+end = time.time()
+
+print("time taken", end - start)
